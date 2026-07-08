caa8b225d84ff01c1b618f731374f7f890933a0e
ImSachin023 <sachinabx3@gmail.com>
2026-07-08 20:11:06 +0530

    APi bug fixes

--- src/components/Body.js
+++ src/components/Body.js
@@ -20,7 +20,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.638211600437955&lng=86.16846609860659&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     );
     const json = await data.json();
     const cards = json?.data?.cards || [];
